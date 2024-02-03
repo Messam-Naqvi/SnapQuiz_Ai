ff7e6a175f9b2a1b73014224a25aba7405fb1f06
Messam-Naqvi <messamnaqvi22@gmail.com>
2024-02-03 18:47:43 +0500

    Final Commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ function App() {
       <div className="heiback">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+
           <Route path="/quiz" element={<Quiz />} />
         </Routes>
       </div>
